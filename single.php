49839b20a3f2ed0231c8c9b1051a3ff9808713aa
Haly Yampolska <halyna.yampolska@gmail.com>
2023-08-27 23:27:22 +0300

    Fixed bags

--- single.php
+++ single.php
@@ -8,18 +8,18 @@
             <div class="content-container">
                 <div class="header__heading heading-primary">
                     <h1 class="heading-1 heading-primary-main mb-sm"><?php the_title(); ?></h1>
-                    <h3 class="heading-2 heading-primary-sub">Subtitle</h3>
                 </div>
             </div>
             </div>
 
             <div class="page-section">
                 <div class="metabox metabox--position-up metabox--with-home-link">
-                    <p>
-                        <a class="metabox__blog-home-link" href="<?php echo site_url('/blog'); ?>"><i class="fa fa-home" aria-hidden="true"></i> Return to Blog </a>
-                        <span class="metabox__main">Posted by <?php the_author_posts_link(); ?> on <?php the_time('n.j.y'); ?> 
-                        in <?php echo get_the_category_list(', '); ?> </span>
-                    </p>
+                <p>
+                    <a class="metabox__blog-home-link" href="<?php echo site_url('/blog'); ?>"><i class="fa fa-home" aria-hidden="true"></i> Return to Blog </a>
+                    <span class="metabox__main">Posted by <?php the_author_posts_link(); ?> on <?php the_time('n.j.y'); ?> 
+                    in <?php the_category(', '); ?> </span>
+                </p>
+
                 </div> 
                 <div class="page-container">
                     <div class="page-content pd-top"> 
